0ba5e67e955aa6b0024f99dcfa79717684881701
Rgsarts <goncalvesramom45@gmail.com>
2026-09-10 01:18:02 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Cliente conectado:', socket.id);
+
+    socket.on('video-frame', (data) => {
+        socket.broadcast.emit('video-frame', data);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log('Servidor rodando...');
+});
